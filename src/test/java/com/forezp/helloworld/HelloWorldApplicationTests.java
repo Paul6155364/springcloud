6b03dad59e35113250848400f786c3d45ca5dd95
yaoxinhong <6155364@qq.com>
2020-09-17 15:25:15 +0800

    提交信息

---
+++ src/test/java/com/forezp/helloworld/HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forezp.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
