ea28e09e76b580c3408644cde2a473a7e8cb72cf
yaoxinhong <6155364@qq.com>
2020-09-21 14:47:39 +0800

    修改

--- src/main/java/com/forezp/helloworld/HelloWorldApplication.java
+++ src/main/java/com/forezp/helloworld/HelloWorldApplication.java
@@ -2,12 +2,18 @@ package com.forezp.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HelloWorldApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HelloWorldApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldApplication.class, args);
+    }
+    @GetMapping("/hi")
+    public String hi(){
+        return "hi,I'm forezp";
+    }
 }
